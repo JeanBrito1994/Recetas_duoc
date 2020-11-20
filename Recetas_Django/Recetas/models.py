c25f72b3e978fd8fe79a0428f9b43e2c83723c65
jean pierre brito castro <jp.brito1994@gmail.com>
2020-11-20 04:46:26 -0300

    dd

--- Recetas_Django/Recetas/models.py
+++ Recetas_Django/Recetas/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
 class usuario(models.Model):
-    rut = models.IntegerField(primary_key=True, max_length=8)
+    rut = models.CharField(primary_key=True, max_length=8)
     rut_dv = models.CharField(max_length=1)
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
