5772f00a3674501cc4ee3c8075251c5784e4d17d
jean pierre brito castro <jp.brito1994@gmail.com>
2020-11-23 14:07:37 -0300

    blocke

--- Recetas_Django/Recetas/views.py
+++ Recetas_Django/Recetas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from Recetas.models import usuarios
 from Recetas.forms import UserCreationForm, registro
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,'Recetas/index.html')
@@ -41,10 +42,16 @@ def formulario(request):
         formulario = registro(request.POST)
         if formulario.is_valid():
             formulario.save()
+            username = formulario.cleaned_data['username']
+            password = formulario.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect ('home')
         else:
             return render(request,'Recetas/formulario.html', {'error':'No se puedo ingresar el usuario'})
 
     return render(request,'Recetas/formulario.html', data)
 
+@login_required
 def administracion(request):
     return render(request,'Recetas/Administracion.html')
--- Recetas_Django/Recetas_Django/urls.py
+++ Recetas_Django/Recetas_Django/urls.py
@@ -1,13 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Recetas import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
-    path('login/', views.login_view,name='login'),
+    path('accounts/login/', views.login_view,name='login'),
     path('recetas/', views.recetas,name='recetas'),
     path('entradas/', views.entradas,name='entradas'),
     path('fondo/', views.fondo,name='fondo'),
@@ -15,5 +15,7 @@ urlpatterns = [
     path('acerca/', views.acerca,name='acerca'),
     path('formulario/', views.formulario,name='formulario'),
     path('administracion/', views.administracion,name='administracion'),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 
 ]
